51b335a1828306c0f00ae54995e80e511d5dc7d6
Riya Bisht <riyabisht716@gmail.com>
2025-11-01 20:35:33 +0530

    Time: 10 ms (93.4%), Space: 50.4 MB (6.4%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,38 +1,35 @@
 class Solution {
-    public int DaysRequired(int [] nums,int capacity){
-        int n = nums.length;
+    public int DaysRequired(int [] weights,int capacity){
+        int n = weights.length;
         int days=1;
         int load = 0;
-        for(int i =0;i< n;i++){
-            if(load + nums[i] >capacity){ // load+nums[i] small ya equal hona chahiye
-                days = days +1;//next day pe jayenge
-                load = nums[i];
+        for(int w :weights){
+            if(load + w  >capacity){ // load+nums[i] small ya equal hona chahiye
+                days++;//next day pe jayenge
+                load = w;
             }
             else{
-                load+= nums[i];//add weight in load
+                load+= w;//add weight in load
             }
            
         }
          return days;
     }
     public int shipWithinDays(int[] weights, int days) {
-        //max element
-        int maxValue = Integer.MIN_VALUE;
-        for(int num:weights){
-            maxValue = Math.max(maxValue,num);
-
-        }
-        //calculating sum
+        //max element and sum
+        int max = 0;
         int sum=0;
-        for(int s: weights){
-            sum+= s;
+          for (int w : weights) {
+            sum += w;
+            if (w > max) max = w;
         }
-        int low = maxValue;
+
+        int low = max;
         int high = sum;
         while(low<=high){
             int mid = low+ (high-low)/2;
-            int noOfDays = DaysRequired(weights,mid);
-            if(noOfDays<= days){
+            int requiredDays = DaysRequired(weights,mid);
+            if(requiredDays<= days){
                 high = mid-1;//possible answer check for smaller one
             }
             else{
