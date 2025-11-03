4f69d1d7171e4255255d0cbeeaef598dd9026f62
Riya Bisht <riyabisht716@gmail.com>
2025-11-03 23:23:14 +0530

    Time: 8 ms (80.34%), Space: 46.2 MB (7.22%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -6,14 +6,14 @@ class Solution {
         }
         
         if(n==1) {
-            return true;//3^0=1
+            return true;//Base case: 3^0=1
         }
         //not divisible by 3 ,not a power of 3
-        if(n%3 !=0){
-            return false;
-        }
+        // if(n%3 !=0){
+        //     return false;
+        // }
         //recursive calls: divide by 3 and check again
-        return isPowerOfThree(n/3);
+        return (n % 3 == 0 && isPowerOfThree(n/3));//check both condition together
         
         
     }
