828bf6ce1056a70dac1e4001eae7b97f93f82016
Riya Bisht <riyabisht716@gmail.com>
2026-01-03 16:14:44 +0530

    Time: 6 ms (84.24%), Space: 44.8 MB (28.52%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,10 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
         String[] words = s.trim().split("\\s+");
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();//to reverse the words
         for(int i = words.length -1;i>=0;i--){
             sb.append(words[i]);
-            if (i != 0) sb.append(" ");
+            if (i != 0) sb.append(" ");//give space only between two words not before
 
         }
         
