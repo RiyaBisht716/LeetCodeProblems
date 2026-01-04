4aa6c4fb9340ea857ace03d28df5794aeb2fabd8
Riya Bisht <riyabisht716@gmail.com>
2026-01-04 23:55:55 +0530

    Time: 0 ms (100%), Space: 48.1 MB (33.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,19 +1,35 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N =n; //to avoid overflow
-        while(N< 0){//negative value ke liye 
-            x = 1/x;//take inverse
-            N= -N;
-        }
-        double ans =1;
-        while(N > 0){ //for positive value
-           if(N %2 ==1) ans *= x; //check for nN=1
-           x*=x;
-           N/=2; 
+        // long N =n; //to avoid overflow
+        // while(N< 0){//negative value ke liye 
+        //     x = 1/x;//take inverse
+        //     N= -N;
+        // }
+        // double ans =1;
+        // while(N > 0){ //for positive value
+        //    if(N %2 ==1) ans *= x; //check for nN=1
+        //    x*=x;
+        //    N/=2; 
 
 
-        }
-        return ans;
-        
+        // }
+        // return ans;
+        double ans = 1.0;
+        long num =n;
+        //handle when  n is negative
+        if(num<0) num = -1 *  num;
+
+        while(num>0){
+            if(num %2==1){
+                ans = ans * x;
+                num= num-1;
+            }
+            else{
+                x = x * x;
+                num = num/2;
+            }
+        }  
+        if(n<0) ans = (double) (1.0)/(double)(ans);
+        return ans;     
     }
 }
