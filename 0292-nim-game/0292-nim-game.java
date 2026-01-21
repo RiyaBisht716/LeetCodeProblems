cd478c59b643d74d0c80dd4f34f0db8ffd17738b
Riya Bisht <riyabisht716@gmail.com>
2026-01-22 01:20:14 +0530

    Time: 0 ms (100%), Space: 42.2 MB (41.66%) - LeetHub

--- 0292-nim-game/0292-nim-game.java
+++ 0292-nim-game/0292-nim-game.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean canWinNim(int n) {
         return n%4 != 0; 
+        // 4 ya 4 ke multiple esa case hai ki mai jeet hi nhi skta  baki ke har case me jeet skta hun
+
+        //me   my friend 
+        // 1 -> 3
+        // 2 -> 2
+        // 3 -> 1
+        // last me my friend remove and he will win
     }
 }
