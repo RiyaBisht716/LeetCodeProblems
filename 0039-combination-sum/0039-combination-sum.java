300a708c8b62ba891ba68aa479b40135623327dd
Riya Bisht <riyabisht716@gmail.com>
2026-01-24 21:12:57 +0530

    Time: 2 ms (94.98%), Space: 45.3 MB (97.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,32 @@
 class Solution {
 
-    public void findCombinations(int idx ,int target,int[]arr,List<List<Integer>> ans ,List<Integer> ds){
-        //Base conditions
-        if(idx == arr.length){
+    public void backtrack(int index, int target,int []arr,List<List<Integer>> ans ,List<Integer> ds){
+        //base case 
+        if(index == arr.length){
             if(target == 0){
                 ans.add(new ArrayList<>(ds));
+            
+
             }
             return;
-
         }
 
-        //to pick the element for combination
-        if(arr[idx]<= target){
-            ds.add(arr[idx]);
-            findCombinations(idx, target-arr[idx] ,arr,ans,ds);
-            ds.remove(ds.size()-1); //backtrack occur impotant to remove the last element
-        }
-        findCombinations(idx+1, target , arr, ans ,ds);
+            if(arr[index] <=target){
+                ds.add(arr[index]);
+                backtrack(index,target-arr[index],arr,ans,ds);
+                ds.remove(ds.size()-1);
+            }
+            //not add current element and move to the index pointer  
+            backtrack(index + 1,target, arr,ans,ds);
+            
+        
+
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();//to stor the final answer
-        List<Integer> ds = new ArrayList<>(); //to store the ds 
-        findCombinations(0,target,candidates,ans,ds);
-        return ans; 
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        backtrack(0,target,candidates,ans,ds);
+        return ans;
+        
     }
 }
