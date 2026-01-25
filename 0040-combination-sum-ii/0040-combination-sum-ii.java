77cf2800dfa0942d1443c46ca7b01dc369dd52d3
Riya Bisht <riyabisht716@gmail.com>
2026-01-25 22:50:02 +0530

    Time: 5 ms (99.06%), Space: 44.9 MB (95.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,27 +1,38 @@
 class Solution {
-    public void findCombination(int idx , int target, int[] arr, List<List<Integer>> result , List<Integer> current){
-        //base condition
-        if(target == 0){
-            result.add(new ArrayList<>(current));
-            return ;
-        }
-        //starting a loop from index to n-1 
-        for(int i =idx;i<arr.length;i++){
+
+    private void findCombination(int index,int target, int[] nums, List<List<Integer>>ans, List<Integer> ds){
+        //base case
+      
+              if(target == 0){
+                ans.add(new ArrayList<>(ds));
+                return;
+                
+             
+            }
+          
+            
+        
             //handle duplicate
-            if(i>idx && arr[i] == arr[i-1]) continue;
-            //the loop will break when current element is greater than target
-            if(arr[i] > target) break;
-            current.add(arr[i]);
-            findCombination(i+1,target-arr[i], arr,result,current);
-            current.remove(current.size()-1);//last elemt from current will be removed
-        }
+            for(int i=index;i<nums.length;i++){
+                if(i >index && nums[i] == nums[i-1]) continue;
+
+                if(nums[i]> target) break;
+
+                ds.add(nums[i]);
+                //for next recursion
+                findCombination(i+1,target-nums[i],nums,ans,ds);
+                ds.remove(ds.size()-1);
+
+            }
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);//sort the array as it is important in duplicates values
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        findCombination(0,target,candidates,result,current);
-        return result;
-      
+        Arrays.sort(candidates);//sorting for handling the duplicate
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+
+        findCombination(0,target,candidates,ans,ds);
+        return ans;
+
+        
     }
 }
