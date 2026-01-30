8cf9d4d424c00ee957c0cdfda8548b880cf8d4ba
Riya Bisht <riyabisht716@gmail.com>
2026-01-30 21:15:11 +0530

    Time: 7 ms (32.63%), Space: 46.9 MB (7.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,33 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        int s1 = s.length();
-        int t1 = t.length();
+    // public boolean isAnagram(String s, String t) {
+    //     int s1 = s.length();
+    //     int t1 = t.length();
 
-        if(s1 != t1) return false;
+    //     if(s1 != t1) return false;
 
-        int [] freq =  new int[26];
+    //     int [] freq =  new int[26];
 
-        for(int i=0;i<s1;i++){
-            freq[s.charAt(i) -'a']++;
-            freq[t.charAt(i)-'a']--;
-        }
+    //     for(int i=0;i<s1;i++){
+    //         freq[s.charAt(i) -'a']++;
+    //         freq[t.charAt(i)-'a']--;
+    //     }
 
-        for(int x : freq){
-            if(x != 0){
-                return false;
-            }
-        }
-        return true;
-    }
+    //     for(int x : freq){
+    //         if(x != 0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+      public boolean isAnagram(String s, String t){
+        char [] s1 = s.toCharArray();
+        char [] t1 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        return Arrays.equals(s1,t1);
+      }
+
+    
 }
