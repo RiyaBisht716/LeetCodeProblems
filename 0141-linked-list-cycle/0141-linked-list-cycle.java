2f45662ae39d8338eabdf28f9d65b3687d6f8175
Riya Bisht <riyabisht716@gmail.com>
2026-02-17 00:24:31 +0530

    Time: 0 ms (100%), Space: 47 MB (14.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,17 +10,53 @@
  * }
  */
 public class Solution {
+
+    // function to detect loop in linked list
+    //detect loop using hashmap - TC- O(N* logN), SC:- N
+    // public boolean detectLoopBrute(ListNode head) {
+    //     // Initialize a pointer 'temp'
+    //     // at the head of the linked list
+    //     ListNode temp = head;
+
+    //     // Create a map to keep track of
+    //     // encountered nodes
+    //     HashMap<ListNode, Integer> nodeMap = new HashMap<>();
+
+    //     // Step 2: Traverse the linked list
+    //     while (temp != null) {
+    //         // If the node is already in the
+    //         // map, there is a loop
+    //         if (nodeMap.containsKey(temp)) {
+    //             return true;
+    //         }
+    //         // Store the current node
+    //         // in the map
+    //         nodeMap.put(temp, 1);
+
+    //         // Move to the next node
+    //         temp = temp.next;
+    //     }
+
+    //     // Step 3: If the list is successfully traversed 
+    //     // without a loop, return false
+    //     return false;
+    // }
+
+    //Detect cycle in a Linked list using tortoise and hare algorithm 
+    //############## Optimal approach:-  TC-(O(N)) , SC :- (O(1))###############
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast !=  null && fast.next != null){
+
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if(slow == fast){
-                return true;
+
+            if(fast == slow){
+                return true;// agar loop detect hoga toh dono pointer ek jagaha meet karenge
             }
         }
-        return false;
+        return false;// agar loop nhi hai 
         
     }
 }
